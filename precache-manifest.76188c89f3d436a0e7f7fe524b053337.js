dd92171fac553317f215f95aba890292a5c19c1f
niloy143 <niloymahmudapu@gmail.com>
2022-09-21 13:56:23 +0600

    Updates

---
+++ precache-manifest.76188c89f3d436a0e7f7fe524b053337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b520074485cd0d602d9",
+    "url": "/another-react-build/static/css/main.79bd9dbe.chunk.css"
+  },
+  {
+    "revision": "9b520074485cd0d602d9",
+    "url": "/another-react-build/static/js/main.9b520074.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/another-react-build/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/another-react-build/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "b8a5cf8dec8b66805b78",
+    "url": "/another-react-build/static/js/runtime~main.b8a5cf8d.js"
+  },
+  {
+    "revision": "1ea10ef464899df3a5790a5b83cc014d",
+    "url": "/another-react-build/index.html"
+  }
+];
