5a6a114476b77c87208c3d55c436dce2897e276f
niloy143 <niloymahmudapu@gmail.com>
2022-09-21 13:59:53 +0600

    no world

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <ExternalUsers></ExternalUsers>
+    <div>
+      <p>Hello World</p>
+      <p>No World</p>
     </div>
   );
-}
-
-function ExternalUsers() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  }, []);
-  return (
-    <div>
-      <h2>External Users</h2>
-      <p>{users.length}</p>
-      {users.map(user => <User name={user.name} email={user.email}></User>)};
-    </div>
-  )
-}
-
-function User(props) {
-  return (
-    <div style={{border: '10px solid black', margin: '5px', borderRadius: '50px'}}>
-      <h3>Name: {props.name}</h3>
-      <p>Email: {props.email}</p>
-    </div>
-  )
-}
-
-function Counter() {
-  let [count, setCount] = useState(55);
-  const increaseCount = () => setCount(++count);
-  const decreaseCount = () => setCount(--count);
-  return (
-    <div>
-      <h3>number: {count}</h3>
-      <button onClick={increaseCount}>increase</button>
-      <button onClick={decreaseCount}>decrease</button>
-    </div>
-  )
-}
+};
 
 export default App;
